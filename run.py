1d80f193ad0f4da46165b5b3bbf02a9b60ef4972
James Evans <james_e75@hotmail.com>
2023-02-03 10:47:15 +0000

    Add game introduction function to welcome use, get user name and display name to user.

--- run.py
+++ run.py
@@ -1,15 +1,24 @@
 import random
 
-def create_card_deck():
+def game_introduction():
     """
-    Creates an ordered deck (list) of playing cards
+    Displays an introduction/ welcome message to the user
+    """
+    print("Welcome to the blackjack game!")
+    name = input("Please enter your player name: ")
+    print(f"Welcome to Blackjack {name}.")
+
+def create_card_pack():
+    """
+    Creates an ordered pack (list) of playing cards
     consisting of (suit symbol, suit name) and
     (card symbol, card points value) and shuffles them.
     """
     suits = [("\u2666", "Diamonds"), ("\u2665", "Hearts"), ("\u2663", "Clubs"), ("\u2660", "Spades")]
     ranks = [("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9), ("10", 10), 
             ("Jack", 10), ("Queen", 10), ("King", 10), ("Ace", 11)]
-    deck = [(suit, rank) for suit in suits for rank in ranks]
-    random.shuffle(deck)
+    pack = [(suit, rank) for suit in suits for rank in ranks]
+    random.shuffle(pack)
 
-create_card_deck()
+game_introduction()
+create_card_pack()
