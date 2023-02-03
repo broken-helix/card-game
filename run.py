31fd2af434b7cd73b690d30aab4cba52911d4e95
James Evans <james_e75@hotmail.com>
2023-02-03 12:52:44 +0000

    Add function to draw two cards for player starting hand

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 import random
 
+pack = []
+player_hand = []
+computer_hand = []
+
 def game_introduction():
     """
-    Displays an introduction/ welcome message to the user
+    Displays an introduction/ welcome message to the user, 
+    asks the user for their name and asks the user if they 
+    would like to read the rules or play the game.
     """
     print("Welcome to the blackjack game!")
     name = input("Please enter your player name: ")
@@ -21,13 +27,7 @@ def display_rules():
     """
     Displays rules of the game
     """
-    print("Display Rules Function")   
-
-def play_game():
-    """
-    Starts the game
-    """
-    print("Play Game Function")     
+    print("Display Rules Function")    
 
 def create_card_pack():
     """
@@ -35,11 +35,26 @@ def create_card_pack():
     consisting of (suit symbol, suit name) and
     (card symbol, card points value) and shuffles them.
     """
+    global pack
     suits = [("\u2666", "Diamonds"), ("\u2665", "Hearts"), ("\u2663", "Clubs"), ("\u2660", "Spades")]
     ranks = [("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9), ("10", 10), 
             ("Jack", 10), ("Queen", 10), ("King", 10), ("Ace", 11)]
     pack = [(suit, rank) for suit in suits for rank in ranks]
     random.shuffle(pack)
+    return pack
+
+def create_player_hand(pack):
+    global player_hand
+    while len(player_hand) < 2:
+        player_hand.append(pack.pop(-1))
+    print(player_hand)
+
+def play_game():
+    """
+    Starts the game
+    """
+    print("Play Game Function")
 
 game_introduction()
 create_card_pack()
+create_player_hand(pack)
