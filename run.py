9d99ada05b03b5dc7d01b6502a2a31f951f5285a
James Evans <james_e75@hotmail.com>
2023-02-10 12:47:33 +0000

    Install pyfiglet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import random
 import sys
+import pyfiglet
 
 suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
 ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
